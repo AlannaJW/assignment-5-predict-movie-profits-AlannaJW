6068ddec95d1dc8fce27aac1a484569b3aa51f3c
Alanna <awroten@westmont.edu>
2023-12-08 14:57:38 -0800

    tests

--- tests/test_data_prep_and_logistic_models.py
+++ tests/test_data_prep_and_logistic_models.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 from src.data_prep import all_classes_prep, edm_rap_prep
-from src.logistic_models import logistic_all_classes, logistic_edm_rap
+from src.logistic_models import logistic_all_classes, logistic_edm_rap, make_y_binary, create_predictions, calc_accuracy
 
 class TestDataPrep(unittest.TestCase):
 
@@ -27,21 +28,40 @@ class TestDataPrep(unittest.TestCase):
         with self.assertRaises(ValueError):
             edm_rap_prep(self.f3)
 
-# class TestLogisticModels(unittest.TestCase):
-#
-#     def setUp(self):
-#
-#
-#     def test_logistic_all_classes_AND_logistic_edm_rap(self):
-#
-#
-#     def test_make_y_binary(self):
-#
-#
-#     def test_create_predictions(self):
-#
-#
-#     def test_calc_accuracy(self):
+class TestLogisticModels(unittest.TestCase):
+
+    def setUp(self):
+        self.y_train_1 = ["rap", "edm", "rap", "edm", "rap", "edm", "rap", "edm"]
+        self.y_class_data_1 = {"rap" : 0, "edm" : 0}
+        self.correct_y_binary = {"rap": [1, 0, 1, 0, 1, 0, 1, 0], "edm" : [0, 1, 0, 1, 0, 1, 0, 1] }
+
+        self.x_test = np.array([[15, 19, 19, 80, 72, 93], [29, 20, 10, 239, 192, 183], [193, 744, 474, 19, 239, 92],
+                                [90, 49, 82, 17, 72, 93], [29, 20, 10, 93, 29, 20], [ 10, 19, 239, 192, 183, 28],
+                                [10, 70, 14, 88, 72, 93], [29, 20, 10, 93, 29, 20]])
+        self.rap_model = LogisticRegression()
+
+        self.rap_model.fit(self.x_test, self.correct_y_binary["rap"])
+        self.edm_model = LogisticRegression()
+        self.edm_model.fit(self.x_test, self.correct_y_binary["edm"])
+        self.class_models = {"rap" : self.rap_model, "edm" : self.edm_model}
+        self.clas_pred = {"edm": 0, "rap": 0}
+        self.predictions = ['rap', 'edm', 'rap', 'edm', 'rap', 'edm', 'rap', 'rap']
+
+    def test_logistic_all_classes_AND_logistic_edm_rap(self):
+        pass
+
+    def test_make_y_binary(self):
+        self.assertEqual(self.correct_y_binary, make_y_binary(self.y_class_data_1, self.y_train_1))
+
+    def test_create_predictions(self):
+        self.assertEqual(self.predictions, create_predictions(self.x_test, self.clas_pred, self.class_models))
+
+    def test_calc_accuracy(self):
+        self.assertEqual(1, calc_accuracy(self.predictions, self.predictions))
+        fifty = ["rap", "edm", "rap", "edm", "edm", "rap", "edm", "edm"]
+        self.assertEqual(0.5, calc_accuracy(fifty, self.predictions))
+        twenty_five = ['rap', 'edm', "edm", 'rap', 'edm', 'rap', 'edm', 'edm']
+        self.assertEqual(0.25, calc_accuracy(twenty_five, self.predictions))
 
 
 if __name__ == '__main__':
